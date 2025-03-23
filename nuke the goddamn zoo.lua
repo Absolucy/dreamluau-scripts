344ab69077cd489dc6e246bf7b30146b938160ee
Lucy <lucy@absolucy.moe>
2025-03-23 11:56:12 -0400

    make this script easier to reuse

--- nuke the goddamn zoo.lua
+++ nuke the goddamn zoo.lua
@@ -2,19 +2,42 @@ local SS13 = require("SS13")
 
 -- set to true to actually delete the mobs.
 -- otherwise it'll just print how many mobs it found
-local actually_delete = false
+local actually_delete = true
 
-local function should_delete(mob)
-	if not SS13.is_valid(mob) then
+-- list of mob typepaths to target
+local target_mobs = {
+	"/mob/living/basic/chicken",
+	"/mob/living/basic/chick"
+}
+
+-- list of area typepaths to target
+local target_areas = {
+	"/area/station/service/hydroponics"
+}
+
+local function is_one_of_type(thing, type_list)
+	if not SS13.is_valid(thing) then
 		return false
 	end
-	if not (SS13.istype(mob, "/mob/living/basic") or SS13.istype(mob, "/mob/living/simple_animal")) then
+	for _, typepath in pairs(type_list) do
+		if SS13.istype(thing, typepath) then
+			return true
+		end
+	end
+	return false
+end
+
+local function should_delete(mob)
+	if not (SS13.is_valid(mob) and SS13.is_valid(mob.loc) and SS13.is_valid(mob.loc.loc)) then
 		return false
 	end
 	if mob.ckey ~= nil or mob.client ~= nil then
 		return false
 	end
-	if not SS13.istype(mob.loc.loc, "/area/station/hallway/primary/fore") then
+	if not is_one_of_type(mob, target_mobs) then
+		return false
+	end
+	if not is_one_of_type(mob.loc.loc, target_areas) then
 		return false
 	end
 	return true
@@ -23,7 +46,7 @@ end
 local total = 0
 
 for _, mob in dm.global_vars.GLOB.mob_living_list do
-	SS13.check_tick()
+	SS13.check_tick(true)
 	if should_delete(mob) then
 		total += 1
 		if actually_delete then
